981742917d9ab87099b328d41b40941d4a45fd20
Alan Adamczyk <alan.adamczyk@adds.nazwa.pl>
2022-07-27 17:04:31 +0200

    set connection to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MongoDB_URI),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
